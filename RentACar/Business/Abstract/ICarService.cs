0d630de80f82616cfd701f2fa8047c115734e9df
BilalOzel7 <mbozel07@gmail.com>
2021-05-31 22:34:16 +0300

    ReCapProject-Backend-
    Recapproject

---
+++ RentACar/Business/Abstract/ICarService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.Results;
+using Entities;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+   public interface ICarService
+    {
+        IDataResult<List<Car>> GetAll();
+        IDataResult<Car> GetByID(int ID);
+        IResult Add(Car entity);
+        IResult Update(Car entity);
+        IResult Delete(Car entity);
+        IDataResult<List<CarDetailDto>> GetCarDetails();
+        IResult AddTransactionalTest(Car car);
+        IDataResult<List<Car>> GetAllByBrandId(int brandId);
+        IDataResult<List<Car>> GetAllByColorId(int colorId);
+        IDataResult<List<CarDetailDto>> GetCarDetailsById(int carId);
+    }
+}
